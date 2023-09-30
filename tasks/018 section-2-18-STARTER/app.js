7257b7de1b8836bdb1440a7b326f90ce441a3483
mohamed ibrahim <mohamedebrahim4221@gmail.com>
2023-09-30 22:56:20 +0300

    solve task

--- tasks/018 section-2-18-STARTER/app.js
+++ tasks/018 section-2-18-STARTER/app.js
@@ -1,2 +1,12 @@
 let vm = Vue.createApp({
+    data() {
+        return {
+            isPurple: false,
+        }
+    },
+    computed: {
+        circleClass(){
+            return { purple: this.isPurple }
+        }
+    }
 }).mount('#app')
