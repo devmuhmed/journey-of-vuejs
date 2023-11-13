e4bebaeeb7d3005e699bfabe134965d573c849b4
mohamed ibrahim <mohamedebrahim4221@gmail.com>
2023-11-13 22:20:19 +0200

    learn with naw v-for

---
+++ tasks/learn with naw/js/script.js
@@ -0,0 +1,34 @@
+const sections = [
+    {
+        id: 1,
+        title: "this is section 1",
+        desc: "this is section 1 descrption"
+    },
+    {
+        id: 2,
+        title: "this is section 2",
+        desc: "this is section 2 descrption"
+    },
+    {
+        id: 3,
+        title: "this is section 3",
+        desc: "this is section 3 descrption"
+    },
+    {
+        id: 4,
+        title: "this is section 4",
+        desc: "this is section 4 descrption"
+    }
+]
+const app = Vue.createApp({
+    // data: function() {
+        data() {
+        return {
+            title: 'my beautiful project 2',
+            desc: 'this is my first vue project',
+            sections
+        }
+    }
+})
+
+app.mount('body');
