fe10e9fe5d9f49984fc09fd2aecc65ef681faefe
mohamed ibrahim <mohamedebrahim4221@gmail.com>
2023-11-18 17:40:21 +0200

    add single quote

--- tasks/learn with naw/vue-cli/single-quote/src/App.vue
+++ tasks/learn with naw/vue-cli/single-quote/src/App.vue
@@ -1,13 +1,20 @@
 <template>
     <page-header title = "myquotes" text="welcome to out website"></page-header>
+    <div class="quotes">
+      <single-quote text="you can do it"></single-quote>
+      <single-quote text="you are good"></single-quote>
+      <single-quote text="well done"></single-quote>
+    </div>
 </template>
 
 <script>
 import PageHeader from './components/PageHeader.vue'
+import SingleQuote from './components/SingleQuote.vue'
 export default {
   name: 'App',
   components: {
-    PageHeader
+    PageHeader,
+    SingleQuote
   }
 }
 </script>
@@ -26,6 +33,10 @@ h1, h2, h3, h4, h5, h6, p {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.quotes {
+  max-width: 80%;
+  margin: 1rem auto;
 }
 </style>
