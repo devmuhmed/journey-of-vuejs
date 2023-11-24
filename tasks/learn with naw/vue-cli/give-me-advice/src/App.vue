6aba53de9a60c84b652938c9a3022699bb41804d
mohamed ibrahim <mohamedebrahim4221@gmail.com>
2023-11-25 00:20:24 +0200

    create project give advice connect style css add pageheader remove helloworld components

---
+++ tasks/learn with naw/vue-cli/give-me-advice/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <page-header 
+  title="Give me advice"
+  text="Provide a number and get apiece of advice"></page-header>
+</template>
+
+<script>
+import PageHeader from './components/PageHeader.vue';
+export default {
+  name: 'App',
+  components: {
+    PageHeader
+  }
+}
+</script>
+
+<style>
+@import './assets/css/style.css';
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
