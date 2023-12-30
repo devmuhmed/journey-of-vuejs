7dc831bd57dfee482d72838fd51b215f60934f2d
mohamed ibrahim <mohamedebrahim4221@gmail.com>
2023-12-30 20:20:02 +0200

    codevolution lesson 11 conditional rendering and export from condition file .vue

---
+++ tasks/codevolution/src/Conditional.vue
@@ -0,0 +1,42 @@
+<template>
+    <h2 v-if="num === 0">The number is zero</h2>
+    <h2 v-else-if="num < 0 ">The number is negative</h2>
+    <h2 v-else-if="num > 0">The number is Positive</h2>
+    <h2 v-else> Not a number </h2>
+
+    //invisible div wraper
+    <template v-if="display">
+        <h2>Mohamed</h2>
+        <h2>Ibrahim</h2>
+        <h2>Hassan</h2>
+    </template>
+
+    <div v-if="display">
+        <h2>Mohamed</h2>
+        <h2>Ibrahim</h2>
+        <h2>Hassan</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ConditionalApp',
+    data() {
+        return {
+            num: 'helo',
+            display: true,
+        }
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- tasks/codevolution/src/main.js
+++ tasks/codevolution/src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
+import ConditionalApp from './Conditional.vue'
 
-createApp(App).mount('#app')
+createApp(ConditionalApp).mount('#app')
