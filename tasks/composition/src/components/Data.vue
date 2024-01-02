3fc7997ee93d93ed76d230de282ec709f8ccaac8
mohamed ibrahim <mohamedebrahim4221@gmail.com>
2024-01-02 22:22:15 +0200

    codevolution lesson 53 the reason to replace data with reactive is repetition

--- tasks/composition/src/components/Data.vue
+++ tasks/composition/src/components/Data.vue
@@ -1,8 +1,14 @@
 <template>
     <div>
         <h2>Options - {{ o_firstName }}</h2>
-        <h2>Options - {{ c_firstName }}</h2>
-        <h2>Options - {{ greet }}</h2>
+        <h2>Options - {{ o_firstName }} {{ o_lastName }} a.k.a {{ o_heroName }}</h2>
+        <hr>
+        <h2>Composition - {{ c_firstName }}</h2>
+        <h2>Composition - {{ greet }}</h2>
+        <hr>
+        <h2>Composition - {{ c_firstName }} {{ c_lastName }} {{ c_heroName }}</h2>
+        <h2>Composition - {{ greetHero }}</h2>
+
     </div>
 </template>
 
@@ -12,16 +18,25 @@ import { ref } from 'vue'
         name: 'DataComponent  ',
         setup() {
            const c_firstName = ref('Clark')
+           const c_lastName = ref('Kent')
+           const c_heroName = ref('Superman')
            c_firstName.value = 'Hassan'
            const greet = `Hello ${c_firstName.value}`
+           const greetHero =`Hello ${c_firstName.value} ${c_lastName.value} a.k.a ${c_heroName.value}`
            return {
             c_firstName,
-            greet
+            greet,
+            c_lastName,
+            c_heroName,
+            greetHero,
+
            }
         },
         data() {
             return {
-                o_firstName: 'Mohamed'
+                o_firstName: 'Mohamed',
+                o_lastName: 'Abdallah',
+                o_heroName: 'Ironman'
             } 
         }
     }
