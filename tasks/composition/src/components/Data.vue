04659dee6ecb6e0433101f6e2fd7c962296ed202
mohamed ibrahim <mohamedebrahim4221@gmail.com>
2024-01-02 22:35:00 +0200

    codevolution lesson 53 replace data with reactive

--- tasks/composition/src/components/Data.vue
+++ tasks/composition/src/components/Data.vue
@@ -8,27 +8,47 @@
         <hr>
         <h2>Composition - {{ c_firstName }} {{ c_lastName }} {{ c_heroName }}</h2>
         <h2>Composition - {{ greetHero }}</h2>
+        <hr>
+        <h2>Reactive {{ state.firstName }} {{ state.lastName }} {{ state.heroName }}</h2>
+        <h2>Reactive {{ reactiveGreetHero }}</h2>
 
     </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
     export default {
         name: 'DataComponent  ',
         setup() {
+            
            const c_firstName = ref('Clark')
            const c_lastName = ref('Kent')
            const c_heroName = ref('Superman')
            c_firstName.value = 'Hassan'
            const greet = `Hello ${c_firstName.value}`
            const greetHero =`Hello ${c_firstName.value} ${c_lastName.value} a.k.a ${c_heroName.value}`
+           const state = reactive({
+                firstName: 'Hisham',
+                lastName: 'Elsayed',
+                heroName: 'my Hero'
+           })
+        //    eleminated to use ref multipletimes and not need to use .value
+           const reactiveGreetHero = `Hello ${state.firstName} ${state.lastName} a.k.a ${state.heroName} `
+
+           // using ref when dealing with primitive types like string boolean number and so on as react can only accept an object
+        //    const isLoggedIn = ref(false)
+        //    const isLoggedInReactive =reactive({
+        //     value: false
+        //    })
+
            return {
             c_firstName,
             greet,
             c_lastName,
             c_heroName,
             greetHero,
+            state,
+            reactiveGreetHero
 
            }
         },
