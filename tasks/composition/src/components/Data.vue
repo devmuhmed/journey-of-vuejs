5aadb1acc512b80934e709a42e21fee1cb05b8bb
mohamed ibrahim <mohamedebrahim4221@gmail.com>
2024-01-02 22:09:20 +0200

    codevolution lesson 51 Replacing data with ref

---
+++ tasks/composition/src/components/Data.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h2>Options - {{ o_firstName }}</h2>
+        <h2>Options - {{ c_firstName }}</h2>
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+    export default {
+        name: 'DataComponent  ',
+        setup() {
+           const c_firstName = ref('Clark')
+           return {
+            c_firstName
+           }
+        },
+        data() {
+            return {
+                o_firstName: 'Mohamed'
+            } 
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
