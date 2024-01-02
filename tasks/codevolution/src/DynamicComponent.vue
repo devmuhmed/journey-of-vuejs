30342808588711a307faa480c6e1e30767a1ffa2
mohamed ibrahim <mohamedebrahim4221@gmail.com>
2024-01-02 12:24:10 +0200

    codevolution lesson 42 dynamic component part two keep alive

--- tasks/codevolution/src/DynamicComponent.vue
+++ tasks/codevolution/src/DynamicComponent.vue
@@ -2,15 +2,16 @@
     <button @click="activeTab = 'TabA'">Tab A</button>
     <button @click="activeTab = 'TabB'">Tab B</button>
     <button @click="activeTab = 'TabC'">Tab C</button>
-
-    <component :is="activeTab" />
+    <keep-alive>
+        <component :is="activeTab" />
+    </keep-alive>
     <!--
     <tab-a v-if="activeTab === 'TabA'" />
     <tab-b v-if="activeTab === 'TabB'" />
     <tab-c v-if="activeTab === 'TabC'" />
     -->
 </template>
-
+ 
 <script>
     import TabA from './components/TabA.vue';
     import TabB from './components/TabB.vue';
