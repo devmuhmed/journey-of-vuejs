e97c95a1daeac9c16efdc1473f7fba886d35cb46
mohamed ibrahim <mohamedebrahim4221@gmail.com>
2024-01-03 05:08:17 +0200

    codevolution lesson 55 replacing mehtods

--- tasks/composition/src/components/Method.vue
+++ tasks/composition/src/components/Method.vue
@@ -2,12 +2,41 @@
     <div>
         <h2> {{ name }} </h2>
         <button @click="setName"> Change Name </button>
+        <hr>
+        <h2>{{ count }}</h2>
+        <button @click="incrementCount">Incerement Count</button>
+        <hr>
+        <h2>{{ firstName }} {{ lastName }}</h2>
+        <button @click="changeHero">Change Hero</button>
     </div>
 </template>
 
 <script>
+    import {ref, reactive,toRefs} from 'vue'
     export default {
         name: 'MethodComponent',
+        setup() {
+            const count = ref(0)
+            function incrementCount() {
+                count.value++
+            }
+            
+            const state = reactive({
+                firstName: 'Mohamed',
+                lastName: 'Hassan'
+            })
+            function changeHero() {
+                state.firstName = 'Hesham'
+                state.lastName = 'Elsayad'
+            }
+            
+            return {
+                count,
+                incrementCount,
+                ...toRefs(state),
+                changeHero
+            }
+        },
         data() {
             return {
                 name: 'Mohamed',
