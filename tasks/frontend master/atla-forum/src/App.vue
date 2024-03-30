26dd51c60e55b5b003b3f15e0c55a9a2398e511f
mohamed ibrahim <mohamedebrahim4221@gmail.com>
2024-03-30 04:14:16 +0200

    props

--- tasks/frontend master/atla-forum/src/App.vue
+++ tasks/frontend master/atla-forum/src/App.vue
@@ -1,5 +1,11 @@
 <script>
+    import BaseCounter from './components/BaseCounter.vue'
+    import BenderStatistics from './components/BenderStatistics.vue'
   export default {
+    components: {
+        BaseCounter,   
+        BenderStatistics,
+    },
             data: ()=> ({
                 newCharacter: {
                     name: '',
@@ -29,27 +35,7 @@
                 ],
                 favoriteList: []
             }),
-            computed:{
-                benderStatistics() {
-                    const elements = ['Air', 'Earth', 'Fire', 'Water']
-                    const statistics = {
-                        Air: 0,
-                        Earth: 0,
-                        Water: 0,
-                        Fire: 0,
-                    }
-
-                    this.characterList.forEach(character => {
-                        elements.forEach(element => {
-                            if(character.element.indexOf(element) > -1) {
-                                statistics[element] += 1 
-                            }
-                        })
-                    })
-
-                    return statistics
-                }
-            },
+            
             methods: {
                 favoriteCharacter(character) {
                     this.favoriteList.push(character)
@@ -63,33 +49,27 @@
 </script>
 
 <template>
-        <h2>Statistics</h2>
-        <ul>
-            <li v-for="(stat, type) in benderStatistics" :key="`bender-${stat}-${type}`">
-                {{ type }}: {{ stat }}
-            </li>
-            {{ benderStatistics }}
-        </ul>
-        <h2>Characters</h2>
-      <p v-if="characterList.length === 0">There are no character</p>
-      <ul v-else>
-        <li v-for="(character, index) in characterList" :key="`even-character-${index}`">
-            <p>
-                {{ character.name }}
-            </p>
-            <button @click="favoriteCharacter(character)">⭐ Favorite</button>
-        </li>
-      </ul>
-      <h2>Favorite Characters</h2>
-      <ul v-if="favoriteList.length > 0">
-        <li v-for="(character, index) in favoriteList" :key="`odd-character-${index}`">
-            {{ character }}
-        </li>
-      </ul>
-      <p v-else>No favorite character</p>
-      <h2>New Character</h2>
-      <pre>{{ newCharacter }}</pre>
-      <label for="character-name">Name</label>
-      <input type="text" v-model="newCharacter.name" @keyup.enter="addNewCharacter">
+    <BaseCounter />
+    <BenderStatistics :characters="characterList" />
+    <h2>Characters</h2>
+    <p v-if="characterList.length === 0">There are no character</p>
+    <ul v-else>
+    <li v-for="(character, index) in characterList" :key="`even-character-${index}`">
+        <p>
+            {{ character.name }}
+        </p>
+        <button @click="favoriteCharacter(character)">⭐ Favorite</button>
+    </li>
+    </ul>
+    <h2>Favorite Characters</h2>
+    <ul v-if="favoriteList.length > 0">
+    <li v-for="(character, index) in favoriteList" :key="`odd-character-${index}`">
+        {{ character }}
+    </li>
+    </ul>
+    <p v-else>No favorite character</p>
+    <h2>New Character</h2>
+    <pre>{{ newCharacter }}</pre>
+    <label for="character-name">Name</label>
+    <input type="text" v-model="newCharacter.name" @keyup.enter="addNewCharacter">
 </template>
-
---
+++ tasks/frontend master/atla-forum/src/components/BaseCounter.vue
@@ -0,0 +1,41 @@
+<script>
+
+export default {
+    data: () => ({
+        count: 10,
+        counterTitle:'Counter Standard',
+        increamentAmount: 8,
+        message: 'Hello it works!',
+        listOfNumbers: [1, 2, 3, 4, 5]
+    }),
+
+    computed: {
+        displayTitle() {
+            return this.count > 20 ? 'Counter Standard - Very Long' : 'Counter Standard'
+        },
+        optimizedIncrementAmount() {
+            return this.displayTitle.length * this.increamentAmount
+        },
+    },
+
+    methods: {
+        increamentCount(newAmount, event) {
+            // this.$data.count
+            console.log(newAmount)
+            console.log(event)
+            this.count += this.optimizedIncrementAmount
+        }
+    },
+        
+}
+</script>
+<template>
+    <h1>{{ displayTitle }}</h1>
+    <p :data-inecrement-by="increamentAmount">{{ count }}</p>
+    <button @click="increamentCount(10, $event)">Increment Count</button>
+    <h1> {{ increamentAmount }}</h1>
+    <p>{{ optimizedIncrementAmount }}</p>
+    <label for="increamentAmount">Increment by:</label>
+    <input type="number" v-model="increamentAmount">
+    <hr>
+</template>
