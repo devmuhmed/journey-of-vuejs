5de7bd186d8bfbb9579d3c0bf55060b0101566f5
mohamed ibrahim <mohamedebrahim4221@gmail.com>
2024-04-14 10:49:06 +0200

    fetching data

--- tasks/frontend master/workshop/src/App.vue
+++ tasks/frontend master/workshop/src/App.vue
@@ -1,11 +1,25 @@
 <script>
-// https://pokeapi.co/api/v2/pokemon/ditto?limit=151
-
+// https://pokeapi.co/api/v2/pokemon?limit=151
+export default {
+  data: () => ({
+    pokedex: [1, 2, 3]
+  }),
+  methods: {
+    async fetchPokemon () {
+      this.pokedex = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+      .then(response => response.json())
+    }
+  }
+}
 </script>
 
 <template>
   <div>
-    hello
+    <h1>New App</h1>
+    <pre>
+      {{ pokedex }}
+    </pre>
+    <button @click="fetchPokemon">Fetch Pokemon</button>
   </div>
 </template>
 
