8b8a30e2040de74a7ebc57f7e27827edf2b2d08c
mohamed ibrahim <mohamedebrahim4221@gmail.com>
2024-05-20 11:48:02 +0300

    solve error package.json problem and add data with old way

--- expense-tracker/src/components/TransactionList.vue
+++ expense-tracker/src/components/TransactionList.vue
@@ -1,13 +1,27 @@
 <template>
     <h3>History</h3>
     <ul id="list" class="list">
-        <li class="minus">
-            cash <span>-$400</span>
-            <button class="delete-btn">X</button>
-        </li>
-        <li class="plus">
-            Paycheck <span>$800</span>
+        <li 
+            v-for="transaction in transactions" :key="transaction.id"
+            :class="transaction.amount < 0 ? 'minus' : 'plus'"
+        >
+            {{ transaction.text }} <span>{{ transaction.amount < 0 ? '-' : '' }} ${{ Math.abs(transaction.amount) }}</span>
             <button class="delete-btn">X</button>
         </li>
     </ul>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                transactions: [
+                    {id:1 , text: 'Flower', amount: -19.99},
+                    {id:1 , text: 'Salary', amount: 299.97},
+                    {id:1 , text: 'Book', amount: -10},
+                    {id:1 , text: 'Camera', amount: 150},
+                ],
+            };
+        },
+    }
+</script>
